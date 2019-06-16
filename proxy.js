17ff16f043a978a79e1ada7d40800b42d78359b4
Ryan Abbott <ryan@element84.com>
2019-06-15 21:49:56 -0400

    Fixes serverless-webpack integration

--- proxy.js
+++ proxy.js
@@ -7,7 +7,11 @@ async function handler(event, context) {
   // extract the path to the handler (relative to the project root)
   // and the function to call on the handler
   const [targetHandlerFile, targetHandlerFunction] = event.targetHandler.split('.');
-  const target = require(path.resolve(__dirname, '../..', targetHandlerFile));
+  console.log(event.targetHandler.split('.'))
+  console.log(targetHandlerFile)
+  console.log(targetHandlerFunction)
+  console.log(event.location)
+  const target = require(path.resolve(__dirname, '../..', event.location, targetHandlerFile));
 
   const targetEvent = JSON.parse(Payload);
   const targetContext = {
