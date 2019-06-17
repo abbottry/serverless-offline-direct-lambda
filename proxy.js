bf9ee4c3416e06f52781003b3823d798a94cb69e
Ryan Abbott <ryan@element84.com>
2019-06-17 13:37:29 -0400

    Fixes serverless-webpack integration

--- proxy.js
+++ proxy.js
@@ -1,51 +1,27 @@
 const serializeError = require('serialize-error');
 const path = require('path');
 
-const awsSerializedError = error => {
-  const { name, message, stack } = serializeError(error)
-  return {
-    errorMessage: message || error,
-    errorName: name,
-    errorStack: stack,
-  }
-}
-
-async function handler(event, context) {
-  const { ClientContext, FunctionName, InvocationType, LogType, Payload } = event.body;
-
+function handler(event, context, callback) {
   // extract the path to the handler (relative to the project root)
   // and the function to call on the handler
-  const [targetHandlerFile, targetHandlerFunction] = event.targetHandler.split('.');
+  const [targetHandlerFile, targetHandlerFunction] = event.targetHandler.split(".");
   const target = require(path.resolve(__dirname, '../..', event.location, targetHandlerFile));
 
-  const targetEvent = JSON.parse(Payload);
-  const targetContext = {
-    ...context,
-  };
-
-  if (ClientContext) {
-    targetContext.clientContext = JSON.parse(Buffer.from(ClientContext, 'base64'));
-  }
-
-  const funcResult = new Promise((resolve, reject) => {
-    const result = target[targetHandlerFunction](targetEvent, targetContext, (error, response) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(response);
-      }
-    });
-
-    if (result && typeof result.then === 'function' && typeof result.catch === 'function') {
-      result.then(resolve).catch(reject);
+  // call the target function
+  target[targetHandlerFunction](event.body, context, (error, response) => {
+    if (error) {
+      callback(null, {
+        StatusCode: 500,
+        FunctionError: 'Handled',
+        Payload: serializeError(error)
+      })
+    } else {
+      callback(null, {
+        StatusCode: 200,
+        Payload: JSON.stringify(response)
+      })
     }
   });
-
-  try {
-    return { StatusCode: 200, Payload: JSON.stringify(await funcResult) };
-  } catch (error) {
-    return { StatusCode: 200, FunctionError: 'Handled', Payload: JSON.stringify(awsSerializedError(error)) };
-  }
 }
 
 module.exports.handler = handler;
